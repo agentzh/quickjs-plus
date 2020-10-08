7ec1392b19bcf6ae2b109cda3e2133c5d6918a6c
Ole André Vadla Ravnås <oleavr@gmail.com>
2020-10-09 01:53:52 +0200

    Add API to support multi-threaded usage
    To allow multiple threads to share the same JSRuntime in lockstep.

--- list.h
+++ list.h
@@ -45,6 +45,11 @@ static inline void init_list_head(struct list_head *head)
     head->next = head;
 }
 
+static inline int list_empty(const struct list_head *el)
+{
+    return el->next == el;
+}
+
 /* insert 'el' between 'prev' and 'next' */
 static inline void __list_add(struct list_head *el, 
                               struct list_head *prev, struct list_head *next)
@@ -67,6 +72,20 @@ static inline void list_add_tail(struct list_head *el, struct list_head *head)
     __list_add(el, head->prev, head);
 }
 
+static inline void list_splice(const struct list_head *list,
+                               struct list_head *head)
+{
+    if (!list_empty(list)) {
+        struct list_head *a = list->next;
+        struct list_head *b = list->prev;
+        struct list_head *c = head->next;
+        head->next = a;
+        a->prev = head;
+        b->next = c;
+        c->prev = b;
+    }
+}
+
 static inline void list_del(struct list_head *el)
 {
     struct list_head *prev, *next;
@@ -76,11 +95,6 @@ static inline void list_del(struct list_head *el)
     next->prev = prev;
     el->prev = NULL; /* fail safe */
     el->next = NULL; /* fail safe */
-}
-
-static inline int list_empty(struct list_head *el)
-{
-    return el->next == el;
 }
 
 #define list_for_each(el, head) \
